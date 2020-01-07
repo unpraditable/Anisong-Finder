fb387cd7670624fb2c04c6f4ef0659ebbd9ef72b
Aditya Pradita <pradita397@gmail.com>
2020-01-07 17:42:31 +0700

    Tidying up layout with bootstrap

--- anisong-finder/src/App.js
+++ anisong-finder/src/App.js
@@ -7,20 +7,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>Anisong Tracker</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
-      <AnimeList />
+      <div className="container">
+        <AnimeList />
+      </div>
     </div>
   );
 }
--- anisong-finder/src/components/AnimeList.js
+++ anisong-finder/src/components/AnimeList.js
@@ -1,6 +1,49 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
+function getSeason(month) {
+    let season ="";
+    switch (month) {
+        case 0:
+            season = "winter";
+            break;
+        case 1:
+            season = "winter";
+            break;
+        case 2:
+            season = "winter";
+            break;
+        case 3:
+            season = "spring";
+            break;
+        case 4:
+            season = "spring";
+            break;
+        case 5:
+            season = "spring";
+            break;
+        case 6:
+          season = "summer";
+          break;
+        case 7:
+            season = "summer";
+            break;
+        case 8:
+            season = "summer";
+            break;
+        case 9:
+            season = "fall";
+            break;
+        case 10:
+            season = "fall";
+            break;
+        case 11:
+            season = "fall";
+            break;
+      }
+      return season;
+}
+
 class AnimeList extends Component {
     state = {
         animes: []
@@ -11,25 +54,24 @@ class AnimeList extends Component {
         .then(res => {
             const animes = res.data.anime;
             this.setState({ animes });
-            console.log(this.state.animes.length);
-
         })
-
-        function isSFW() {
-            
-        }
     }
+
+    
 
     render () {
         return (
-            <div>
-
+            <div className="App-body row">
             { 
                 this.state.animes.map(anime =>
-                <div>
-                    <img src={anime.image_url} />
-                    <h3>{anime.title}</h3>
-                </div>
+                
+                anime.r18 === false ? (
+                    <div className ="col-md-4">
+                        <img src={anime.image_url} />
+                        <h3>{anime.title}</h3>
+                    </div>
+                ) : (null)
+                
                 )
             }
             
